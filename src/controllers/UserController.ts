dcb03ba20868b08ce08625f26e10e9a3355e46d9
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2024-02-04 19:22:59 -0300

    feat: user controller and routes

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,96 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+import { Request, Response } from "express";
+import auth from "../config/auth";
+import filtrarDadosDoCliente from "../utils/filterUserData";
+
+class UserController {
+  async createUser(req: Request, res: Response) {
+    try {
+      const {
+        firstName,
+        secondName,
+        email,
+        password,
+        cpf,
+        phone,
+        birthDate,
+        cep,
+        address,
+        addressRef = null,
+      } = req.body;
+      const { hash, salt } = auth.generatePassword(password);
+      const data = {
+        firstName,
+        secondName,
+        email,
+        cpf,
+        phone,
+        birthDate: new Date(birthDate).toISOString(),
+        cep,
+        address,
+        addressRef,
+        hash,
+        salt,
+      };
+
+      const user = await prisma.user.create({
+        data: data,
+      });
+
+      res.status(201).json(filtrarDadosDoCliente(user));
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error });
+    }
+  }
+
+  async getAllUsers(req: Request, res: Response) {
+    try {
+      const user = await prisma.user.findMany();
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+
+  async getUser(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const user = await prisma.user.findUnique({
+        where: { id: id },
+      });
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+
+  async updateUser(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const cliente = await prisma.user.update({
+        where: { id: id },
+        data: req.body,
+      });
+      res.status(200).json(cliente);
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+
+  async deleteUser(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const user = await prisma.user.delete({
+        where: { id: id },
+      });
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+}
+
+export default new UserController();
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,1 +1,16 @@
+import { Router } from "express";
+const router = Router();
 
+import UserController from "../controllers/UserController";
+import AuthController from "../controllers/AuthController";
+
+router.post("/login", AuthController.login);
+router.get("/user", AuthController.getDetails);
+
+router.post("/user", UserController.createUser);
+router.get("/users", UserController.getAllUsers);
+router.get("/user/:id", UserController.getUser);
+router.put("/user/:id", UserController.updateUser);
+router.delete("/user/:id", UserController.deleteUser);
+
+export default router;
