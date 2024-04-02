3d74eccccdb8f6742ffee70bc1ae3a9ce730070d
Gabriel Ayres <Gabriel.ar.fort@gmail.com>
2024-04-02 15:02:26 -0300

    feat: expiration date validation

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,8 +1,15 @@
-import jsonwebtoken from "jsonwebtoken";
+import jsonwebtoken, { JwtPayload, TokenExpiredError, decode } from "jsonwebtoken";
 import crypto from "crypto";
-import { Request } from "express";
+import { Request, Response } from "express";
 import * as fs from "fs";
 import * as path from "path";
+import { Token } from "nodemailer/lib/xoauth2";
+
+type TokenDecoded = {
+  sub: number,
+  iat: number,
+  exp: number,
+} | string | jsonwebtoken.JwtPayload
 
 const PRIV_KEY = fs.readFileSync(
   path.join(__dirname, "..", "..", "id_rsa_priv.pem"),
@@ -28,12 +35,66 @@ const generateJWT = (user: any, time?: string) => {
     iat: Date.now(),
   };
 
+  console.log(time)
+
   const jwt = jsonwebtoken.sign(payload, PRIV_KEY, {
     expiresIn: time ? time : "7d",
     algorithm: "RS256",
   });
+
   return jwt;
 };
+
+/* const generateJWTTest = (req: Request, res: Response) => {
+  const user = {
+    id: 324235432,
+    name: "Gabriel"
+  }
+  const sub = user.id;
+  const payload = {
+    sub: sub,
+    iat: Date.now(),
+  }
+  const jwt = jsonwebtoken.sign(payload, PRIV_KEY, {
+    expiresIn: "21d",
+    algorithm: "RS256"
+  })
+
+  const decode = decodeJWT(jwt)
+  const {exp} = decode
+
+  console.log(decode.exp - decode.iat)
+  
+  return res.status(200).send(jwt)
+} */
+
+const validateJWT = (req: Request, res: Response) => {
+  const {token} = req.body
+  const decoded = jsonwebtoken.verify(token, PRIV_KEY) as JwtPayload
+  if(decoded.iat && decoded.exp) {
+    const expired = isTokenExpired(decoded.iat, decoded.exp)
+    if(!expired) return res.status(200).json(decoded)
+    else return res.status(401).json("Invalid token")
+  }
+}
+
+const isTokenExpired = (iat: number, exp: number) => {
+  const duration = exp - iat
+  const beg = new Date(iat)
+  const limit = new Date(beg.getTime() + duration * 1000)
+  return (new Date() > limit)
+}
+
+/* const isTokenExpired = (token: string) => {
+  const payload = decodeJWT(token)
+  const { exp } = decodeJWT(token);
+  const date = new Date(exp)
+  const expirationTimeInSeconds = exp * 1000; // converting expiration time to milliseconds
+  console.log(payload)
+  console.log(date)
+  const currentTime = Date.now();
+  return expirationTimeInSeconds < currentTime;
+}; */
 
 const checkPassword = (password: string, hash: string, salt: string) => {
   const hashFromRequest = crypto
@@ -42,12 +103,12 @@ const checkPassword = (password: string, hash: string, salt: string) => {
   return hashFromRequest === hash;
 };
 
-const decodeJWT = (token: any) => {
+/* const decodeJWT = (token: any) => {
   const payload = token.split(".")[1];
   const encodedPayload = Buffer.from(payload, "base64");
   const decodePayload = encodedPayload.toString("utf-8");
   return JSON.parse(decodePayload);
-};
+}; */
 
 const getToken = (req: Request) => {
   let token = null;
@@ -82,6 +143,7 @@ export default {
   generatePassword,
   generateJWT,
   checkPassword,
-  decodeJWT,
+  isTokenExpired,
   getToken,
+  validateJWT,
 };
